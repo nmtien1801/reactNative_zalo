3ec7ea38898db207a399cba8b889e9b9676b61c7
Loc <nguyentanloc13c9@gmail.com>
2025-03-02 14:21:56 +0700

    update chat screen, add image into assets

--- ChatScreen.js
+++ ChatScreen.js
@@ -12,10 +12,10 @@ const MENU_WIDTH = width * 0.5;
 const MENU_HEIGHT = height * 0.3;
 
 const messages = [
-  { id: "1", name: "Media Box", message: "Zing MP3: Lắng nghe Negav...", time: "", avatar: "https://via.placeholder.com/50" },
-  { id: "2", name: "Hiếu Lai", message: "cho xin UML của m đi", time: "2 giờ", avatar: "https://via.placeholder.com/50" },
-  { id: "3", name: "TTDT_NHÓM 1_TH", message: "Nhắc hẹn: deadline đi...", time: "5 giờ", avatar: "https://via.placeholder.com/50" },
-  { id: "4", name: "Hồ Mờ Hồ Và Những Người Bạn", message: "Hồ Minh Hậu: T về LA", time: "6 giờ", avatar: "https://via.placeholder.com/50" },
+  { id: "1", name: "Media Box", message: "Zing MP3: Lắng nghe Negav...", time: "", avatar: require("./assets/man.png")  },
+  { id: "2", name: "Hiếu Lai", message: "cho xin UML của m đi", time: "2 giờ", avatar: require("./assets/man.png") },
+  { id: "3", name: "TTDT_NHÓM 1_TH", message: "Nhắc hẹn: deadline đi...", time: "5 giờ", avatar: require("./assets/man.png") },
+  { id: "4", name: "Hồ Mờ Hồ Và Những Người Bạn", message: "Hồ Minh Hậu: T về LA", time: "6 giờ", avatar: require("./assets/man.png")},
 ];
 
 const ChatScreen = () => {
@@ -89,7 +89,7 @@ const ChatScreen = () => {
           keyExtractor={(item) => item.id}
           renderItem={({ item }) => (
             <TouchableOpacity style={{ flexDirection: "row", alignItems: "center", padding: 15, backgroundColor: "white", marginBottom: 2, height: ITEM_HEIGHT }}>
-              <Image source={{ uri: item.avatar }} style={{ width: AVATAR_SIZE, height: AVATAR_SIZE, borderRadius: AVATAR_SIZE / 2, marginRight: 15 }} />
+              <Image source={item.avatar} style={{ width: AVATAR_SIZE, height: AVATAR_SIZE, borderRadius: AVATAR_SIZE / 2, marginRight: 15 }} />
               <View style={{ flex: 1 }}>
                 <Text style={{ fontWeight: "bold", fontSize: 16 }}>{item.name}</Text>
                 <Text style={{ color: "gray" }}>{item.message}</Text>
